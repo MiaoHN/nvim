f8f73c5e7bbce5e801f0f7b74ab9eaf6e2aeea62
MiaoHN <582418227@qq.com>
2021-11-02 17:02:16 +0800

    config colorscheme

--- lua/plugins/galaxyline/init.lua
+++ lua/plugins/galaxyline/init.lua
@@ -1,8 +1,95 @@
 local gl = require('galaxyline')
-local colors = require('galaxyline.theme').default
-local condition = require('galaxyline.condition')
 local gls = gl.section
-gl.short_line_list = {'NvimTree','vista','dbui','packer'}
+local condition = require('galaxyline.condition')
+-- local utils = require('galaxyline-config.utils')
+gl.short_line_list = {'NvimTree','vista', 'dashboard','dbui','packer'}
+
+local get_colors = function()
+  if vim.g.colors_name == 'onedark' then
+    local colors = {
+      lightbg = '#38393f',
+      black = "#2a2e36",
+      red = "#e06c75",
+      green = "#93c379",
+      yellow = "#e5c07b",
+      blue = "#61afef",
+      purple = "#c678dd",
+      cyan = "#56b6c2",
+      white = "#cfcfcf"
+    }
+    return colors
+  elseif vim.g.colors_name == 'tokyonight' then
+    local colors = {
+      lightbg = "#414868",
+      black = "#24283b",
+      red = "#f7768e",
+      green = "#9ece6a",
+      yellow = "#e0af68",
+      blue = "#7aa2f7",
+      purple = "#bb9af7",
+      cyan = "#2ac3de",
+      white = '#d5d6db'
+    }
+    return colors
+  elseif vim.g.colors_name == 'nord' then
+    local colors = {
+      lightbg = '#4C566A',
+      black = "#3B4252",
+      red = "#BF616A",
+      green = "#A3BE8C",
+      yellow = "#EBCB8B",
+      blue = "#81A1C1",
+      purple = "#B48EAD",
+      cyan = "#88C0D0",
+      white = "#E5E9F0"
+    }
+    return colors
+  elseif vim.g.colors_name == 'custom' then
+    local colors = {
+      lightbg = "#414868",
+      black = "#24283b",
+      red = "#f7768e",
+      green = "#9ece6a",
+      yellow = "#e0af68",
+      blue = "#7aa2f7",
+      purple = "#bb9af7",
+      cyan = "#2ac3de",
+      white = '#d5d6db'
+    }
+    return colors
+  elseif vim.g.colors_name == 'gruvbox' then
+    local colors = {
+      lightbg = "#504945",
+      black = '#272727',
+      red = "#cc231c",
+      green = "#989719",
+      yellow = "#d79920",
+      blue = "#448488",
+      purple = "#b16185",
+      cyan = "#689d69",
+      white = "#ebdbb2"
+    }
+    return colors
+  elseif vim.g.colors_name == 'dracula' then
+    local colors = {
+      lightbg = "#44475a",
+      black = '#282a36',
+      red = "#ff5555",
+      green = "#50fa7b",
+      yellow = "#f1fa8c",
+      blue = "#6272a4",
+      purple = "#bd93f9",
+      cyan = "#8be9fd",
+      white = "#f8f8f2"
+    }
+    return colors
+  else
+    local colors = require('galaxyline.theme').default
+    return colors;
+  end
+end
+
+local colors = get_colors();
 
 gls.left[1] = {
   RainbowRed = {
