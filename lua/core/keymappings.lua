f1b1be8e2feb72f55e981f01259316bdffbcaf3a
MiaoHN <582418227@qq.com>
2021-12-10 13:27:33 +0800

    [refract] change keymappings.lua

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -1,57 +1,63 @@
--- disable <Space> in normal mode
-vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", { noremap = true, silent = true })
-
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+local opt = {
+	noremap = true,
+	silent = true,
+}
+-- disable <Space> in normal mode
+map("n", "<Space>", "<NOP>", { noremap = true, silent = true })
 
 -- explore tree
-vim.api.nvim_set_keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+map("n", "<Leader>e", ":NvimTreeToggle<CR>", opt)
 
 -- hl
-vim.api.nvim_set_keymap("n", "<Leader><Leader>", ":set hlsearch!<CR>", { noremap = true, silent = true })
+map("n", "<Leader><Leader>", ":set hlsearch!<CR>", opt)
 
 -- window movement
-vim.api.nvim_set_keymap("n", "<Leader>h", "<C-w>h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>j", "<C-w>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>k", "<C-w>k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>l", "<C-w>l", { noremap = true, silent = true })
+map("n", "<Leader>h", "<C-w>h", opt)
+map("n", "<Leader>j", "<C-w>j", opt)
+map("n", "<Leader>k", "<C-w>k", opt)
+map("n", "<Leader>l", "<C-w>l", opt)
 
 -- switch
-vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
+map("n", "<TAB>", ":bnext<CR>", opt)
+map("n", "<S-TAB>", ":bprevious<CR>", opt)
 
 -- move selected line /block of the text in visual mode
-vim.api.nvim_set_keymap("x", "K", ":move '<-2<CR>gv-gv'", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "J", ":move '>+1<CR>gv-gv'", { noremap = true, silent = true })
+map("x", "K", ":move '<-2<CR>gv-gv'", opt)
+map("x", "J", ":move '>+1<CR>gv-gv'", opt)
 
 -- Esc
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+map("i", "jj", "<Esc>", opt)
 
 -- better movement
-vim.api.nvim_set_keymap("n", "J", "5j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "K", "5k", { noremap = true, silent = true })
+map("n", "J", "5j", opt)
+map("n", "K", "5k", opt)
 
 -- tab complete
-vim.api.nvim_set_keymap("i", "<expr><TAB>", 'pumvisible() ? "\\<c-n>" : "\\<TAB>"', { noremap = true, silent = true })
+map("i", "<expr><TAB>", 'pumvisible() ? "\\<c-n>" : "\\<TAB>"', opt)
 
 -- undo
-vim.api.nvim_set_keymap("n", "U", ":UndotreeToggle<CR>", { noremap = true, silent = true })
+map("n", "U", ":UndotreeToggle<CR>", opt)
 
 -- markdown
--- vim.api.nvim_set_keymap('n', '<C-V>', ':PasteImg<CR>', { noremap = true,silent = true })
+-- map('n', '<C-V>', ':PasteImg<CR>', opt)
 
 -- comment quicker
-vim.api.nvim_set_keymap("n", "<Leader>c", ":CommentToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<Leader>c", ":CommentToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<Leader>c", ":CommentToggle<CR>", { noremap = true, silent = true })
+map("n", "<Leader>c", ":CommentToggle<CR>", opt)
+map("v", "<Leader>c", ":CommentToggle<CR>", opt)
+map("x", "<Leader>c", ":CommentToggle<CR>", opt)
 
 -- Vista
-vim.api.nvim_set_keymap("n", "<Leader>v", ":Vista!!<CR>", { noremap = true, silent = true })
+map("n", "<Leader>v", ":Vista!!<CR>", opt)
 
 -- lazygit
-vim.api.nvim_set_keymap("n", "<Leader>g", ":LazyGit<CR>", { noremap = true, silent = true })
+map("n", "<Leader>g", ":LazyGit<CR>", opt)
 
 -- bookmarks
-vim.api.nvim_set_keymap("n", "<Leader>b", ":BookmarkToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>B", ":BookmarkShowAll<CR>", { noremap = true, silent = true })
+map("n", "<Leader>b", ":BookmarkToggle<CR>", opt)
+map("n", "<Leader>B", ":BookmarkShowAll<CR>", opt)
 
 -- coc.nvim
